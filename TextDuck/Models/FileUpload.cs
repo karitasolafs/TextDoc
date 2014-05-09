207c5f2b24f79d5f6d8efce5431452f304fa6399
johannai13 <johannai13@ru.is>
2014-05-09 01:54:06 +0100

    Hafa samband lagad, linkar lagadir, fieldset sett allsstadar sem vid a, ollu breytt i razor i fileapperance

--- TextDuck/Models/FileUpload.cs
+++ TextDuck/Models/FileUpload.cs
@@ -12,17 +12,21 @@ namespace TextDuck.Models
         public int? FileId { get; set; }
 
         [Required(ErrorMessage = "Title is required.")]
+        [Display(Name = "Nafn: ")]
         public string FileTitle { get; set; }
 
         [ValidateFileAttribute(ErrorMessage = "Please select a .srt file")]
+        [Display(Name = "Velja skra: ")]
         public HttpPostedFileBase File { get; set; }
 
         public DateTime FileDate { get; set; }
 
         [Required(ErrorMessage = "Category is required.")]
+        [Display(Name = "Flokkur: ")]
         public string FileCategory { get; set; }
 
         [Required(ErrorMessage = "Genre is required.")]
+        [Display(Name = "Tegund: ")]
         public string FileGenre { get; set; }
     }
 }
