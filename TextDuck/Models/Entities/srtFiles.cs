616a88512a3cbdf3bc22a5089b4e54706a35d7fc
joigisla <joigisla91@hotmail.com>
2014-05-09 21:43:29 +0000

    srtFiles

---
+++ TextDuck/Models/Entities/srtFiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TextDuck.Models
+{
+    public class srtFiles
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public string Genre { get; set; }
+    }
+}
