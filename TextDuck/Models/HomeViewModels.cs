48b0c50873feb525c533d6ac1d57979d75177ed6
joigisla <joigisla91@hotmail.com>
2014-05-10 10:33:37 +0000

    bla

---
+++ TextDuck/Models/HomeViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TextDuck.Models
+{
+    public class SubtitlesViewModel
+    {
+        [Required]
+        [Display(Name = "Flokkur")]
+        public string Category { get; set; }
+
+    }
+}
