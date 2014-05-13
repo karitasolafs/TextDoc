a4806186d67e6c75bda12b58a2fae41b3e5737cf
johannai13 <johannai13@ru.is>
2014-05-14 00:32:18 +0100

    Leitarvel tilbuin

--- TextDuck/Controllers/HomeController.cs
+++ TextDuck/Controllers/HomeController.cs
@@ -290,6 +290,20 @@ namespace TextDuck.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult SearchResult(string query)
+        {
+            if (!String.IsNullOrEmpty(query))
+            {
+                var all = repo.GetAllFiles();
+                var result = (from item in all
+                              orderby item.Date
+                              where item.Title.Contains(query)
+                              select item);
+                return View(result);
+            }
+            return View("Index");
+        }
 
     }
 }
