7dd197c443a3e8eb3f0528dbfc02e86f16507299
thorhildur13 <thorhildur13@ru.is>
2014-05-14 20:24:45 +0000

    g

--- TextDuck/Controllers/HomeController.cs
+++ TextDuck/Controllers/HomeController.cs
@@ -108,6 +108,7 @@ namespace TextDuck.Controllers
             var comment = Comment.GetNews();
             return View(comment);
         }
+        //gaman saman
         public void AddVote(int Id)
         {
 
